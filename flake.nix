abe288a9b6ef0823be30f24c4b8b5f7f3de90dc1
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2023-06-02 00:13:06 +0300

    flake: bump nixpkgs to 23.05

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
